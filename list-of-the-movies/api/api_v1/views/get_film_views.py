e8d59168866587be0847827dfe0fd8ff088a052a
GlebKostousov <kostousjr@gmail.com>
2025-04-27 21:18:15 +0300

    указал response_model

--- list-of-the-movies/api/api_v1/views/get_film_views.py
+++ list-of-the-movies/api/api_v1/views/get_film_views.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import (
     HTTPException,
     APIRouter,
@@ -21,11 +23,11 @@ def get_film_by_id(movie_id: int) -> Movie | None:
     raise HTTPException(status_code=404, detail=f"Не найден фильм с id={movie_id}")
 
 
-@router.get("/{movie_id}")
+@router.get("/{movie_id}", response_model=Movie)
 def read_movie(movie_id: int):
     return get_film_by_id(movie_id)
 
 
-@router.get("/list")
+@router.get("/list", response_model=List[Movie])
 def read_list_of_films():
     return MOVIES_LIST
