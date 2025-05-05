4b95e0ee39215f73d383924b8357bddb808b4317
GlebKostousov <kostousjr@gmail.com>
2025-05-05 14:16:39 +0300

    Переделал работу с json для имитации бд

--- list-of-the-movies/services/tools.py
+++ list-of-the-movies/services/tools.py
@@ -1,12 +1,6 @@
 from pathlib import Path
-import sys
 
 
-# from typing import Type
-# import json
-# from pydantic import BaseModel
-#
-#
 def get_project_root() -> Path:
     """
     Находит корень проекта через файл main.py в родительских директориях.
@@ -20,27 +14,3 @@ def get_project_root() -> Path:
         if parent == root:  # Достигли корня файловой системы
             raise FileNotFoundError("Файл main.py не найден.")
         root = parent
-
-
-#
-#
-# def save_to_json(data: Type[BaseModel], file: str) -> None:
-#     """
-#     Сохраняет данные в JSON-файл.
-#     Если данные являются датаклассом, они преобразуются в словарь с помощью asdict.
-#
-#     """
-#     # Добавляем расширение .json к имени файла
-#     file += ".json"
-#
-#     # Получаем корневой путь проекта
-#     root = get_project_root()
-#     file_path = root / file
-#
-#     try:
-#         with data.model_dump_json(...) as json_file:
-#         # with open(file_path, "w", encoding="utf-8") as json_file:
-#         #     json.dump(data, json_file, ensure_ascii=False, indent=4)
-#
-#     except Exception as e:
-#         raise
