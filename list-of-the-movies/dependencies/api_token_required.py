e3b94e29f2254762374827497643891ee0b12873
GlebKostousov <kostousjr@gmail.com>
2025-05-09 15:42:00 +0300

    Сделал вход в библиотеку через Bearer токен

--- list-of-the-movies/dependencies/api_token_required.py
+++ list-of-the-movies/dependencies/api_token_required.py
@@ -1,27 +1,50 @@
 from fastapi import (
     Request,
     status,
-    Header,
     HTTPException,
 )
 from typing import Annotated
+
+from fastapi.params import Depends
+
 from services.const import (
     UNSAFE_METHODS,
     API_TOKENS,
+)
+
+from fastapi.security import (
+    HTTPBearer,
+    HTTPAuthorizationCredentials,
+)
+import logging
+
+log = logging.getLogger(__name__)
+
+static_api_token = HTTPBearer(
+    scheme_name="Static API Token",
+    description="Your **API token from developer** portal. [Readme more](#)",
+    auto_error=False,
 )
 
 
 def api_token_required(
     request: Request,
     api_token: Annotated[
-        str,
-        Header(alias="x-auth-token"),
-    ] = "",
+        HTTPAuthorizationCredentials | None,
+        Depends(static_api_token),
+    ] = None,
 ):
+    log.info("Received %r API token", api_token)
     if request.method not in UNSAFE_METHODS:
         return
 
-    if api_token not in API_TOKENS:
+    if not api_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Empty API token",
+        )
+
+    if api_token.credentials not in API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="You APIkey invalid",
