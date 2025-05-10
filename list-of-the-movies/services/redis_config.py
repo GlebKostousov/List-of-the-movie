3f1c91f11621d2c6f9d1b18d796f4fa491ed6092
GlebKostousov <kostousjr@gmail.com>
2025-05-10 14:43:34 +0300

    Сделал соединение с redis в проекте

---
+++ list-of-the-movies/services/redis_config.py
@@ -0,0 +1,10 @@
+"""
+Настройки для подключения Redis
+"""
+
+from typing import Final
+
+REDIS_HOST: Final[str] = "localhost"
+REDIS_PORT: Final[int] = 6379
+REDIS_DB: Final[int] = 0
+REDIS_DECODE: Final[bool] = True
