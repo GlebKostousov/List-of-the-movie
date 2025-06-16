6dc3856c98d54084ea9ee4dedb2bbc68fd42bd24
GlebKostousov <kostousjr@gmail.com>
2025-06-16 09:21:03 +0300

    сделал проверку функции генерации, генерации и сохранения и проверки на существование токена

---
+++ list-of-the-movies/testing/test_api/test_api_v1/test_service/test_auth/test_redis_auth/test_redis_token_helper.py
@@ -0,0 +1,18 @@
+from os import getenv
+from unittest import TestCase
+
+from api.api_v1.service.auth.redis_auth import redis_token
+
+if getenv("TESTING") != "1":
+    error_testing_msg = "Environment is not ready for testing"
+    raise OSError(error_testing_msg)
+
+
+class TestRedisTokenHelper(TestCase):
+    def test_generate_and_save_token(self) -> None:
+        new_token = redis_token.generate_and_save_token()
+        expected_exists = True
+        self.assertEqual(
+            expected_exists,
+            redis_token.token_exists(token_to_check=new_token),
+        )
