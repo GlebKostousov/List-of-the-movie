1256a674e78021be5a5d62d4517a2feba5649a26
GlebKostousov <kostousjr@gmail.com>
2025-11-02 20:27:35 +0300

    test(TestCreateInvalid): create invalid value for create movie api

--- list-of-the-movies/testing/test_api/test_api_v1/test_views/test_movie_create.py
+++ list-of-the-movies/testing/test_api/test_api_v1/test_views/test_movie_create.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+import pytest
+from _pytest.fixtures import SubRequest
 from fastapi import status
 from starlette.testclient import TestClient
 from main import app
@@ -29,3 +33,33 @@ def test_create_movie_already_exists(auth_client: TestClient, movie: Movie) -> N
     response_detail = response.json()["detail"]
     expected_detail = f"Movie with slug={movie.slug!r} already exists"
     assert response_detail == expected_detail, response_detail
+
+
+class TestCreateInvalid:
+
+    @pytest.fixture(
+        params=[
+            pytest.param(("12", "string_too_short"), id="short-slug"),
+            pytest.param(("s" * 31, "string_too_long"), id="long-slug"),
+        ]
+    )
+    def movie_create_values(self, request: SubRequest) -> tuple[dict[str, Any], str]:
+        slug, error_msg = request.param
+        build = build_movie_create_random_slug()
+        data = build.model_dump(mode="json")
+        data["slug"] = slug
+        return data, error_msg
+
+    def test_invalid_slug(
+        self,
+        movie_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_movie")
+        data, error_msg = movie_create_values
+        response = auth_client.post(url=url, json=data)
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        response_detail = response.json()["detail"]
+        assert response_detail[0]["type"] == error_msg, response_detail
