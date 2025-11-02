84ec0bec5652290a749befbcc8e8fdfd4bf7cac3
GlebKostousov <kostousjr@gmail.com>
2025-11-02 23:06:49 +0300

    test(conftest): add auto fixture to check ENV

--- list-of-the-movies/testing/conftest.py
+++ list-of-the-movies/testing/conftest.py
@@ -2,6 +2,9 @@ from os import getenv
 
 import pytest
 
-if getenv("TESTING") != "1":
-    error_testing_msg = "Environment is not ready for testing"
-    pytest.exit(error_testing_msg)
+
+@pytest.fixture(scope="session", autouse=True)
+def check_testing_env() -> None:
+    if getenv("TESTING") != "1":
+        error_testing_msg = "Environment is not ready for testing"
+        pytest.exit(error_testing_msg)
--- list-of-the-movies/testing/test_api/test_api_v1/test_service/test_auth/test_redis_auth/test_redis_token_helper.py
+++ list-of-the-movies/testing/test_api/test_api_v1/test_service/test_auth/test_redis_auth/test_redis_token_helper.py
@@ -3,10 +3,6 @@ from unittest import TestCase
 
 from api.api_v1.service.auth.redis_auth import redis_token
 
-if getenv("TESTING") != "1":
-    error_testing_msg = "Environment is not ready for testing"
-    raise OSError(error_testing_msg)
-
 
 class TestRedisTokenHelper(TestCase):
     def test_generate_and_save_token(self) -> None:
