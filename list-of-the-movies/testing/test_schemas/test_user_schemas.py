17ce13c199a35eebed8b631b855cc63cc4ca734e
GlebKostousov <kostousjr@gmail.com>
2025-11-04 13:47:08 +0300

    test(test-user-schema): create skip test

---
+++ list-of-the-movies/testing/test_schemas/test_user_schemas.py
@@ -0,0 +1,27 @@
+import pathlib
+import sys
+import os
+import pytest
+
+
+@pytest.mark.skip(reason="user schema not implemented yet")
+def test_user_schema() -> None:
+    user_data = {"username": "foobar"}
+    assert user_data["username"] == "spam"
+
+
+@pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="skip test due to some reason",
+)
+def test_platform() -> None:
+    assert sys.platform != "win32"
+
+
+@pytest.mark.skipif(
+    os.name != "nt",
+    reason="run only on windows",
+)
+def test_only_for_windows() -> None:
+    path = pathlib.Path(__file__)
+    assert isinstance(path, pathlib.WindowsPath)
